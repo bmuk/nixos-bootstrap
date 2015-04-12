639303db72398e88a5b0771aa7a4c7cc135c95b5
Britt Mathis <britt.mathis@gmail.com>
2015-04-12 11:37:25 -0400

    Fixed configuration.nix bug
    Still needs networking.hostId to be set

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
-  boot.loader.supportedFilesystems = [ "zfs" ];
+  boot.supportedFilesystems = [ "zfs" ];
   networking.hostName = "crocodile";
   networking.wireless.enable = true;
 }
