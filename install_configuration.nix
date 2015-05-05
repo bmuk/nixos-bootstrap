1c3716f1f457c319d040d353bb5c9755e376a022
Britt Mathis <britt.mathis@gmail.com>
2015-05-05 19:42:28 -0400

    fixed live environment bug

--- install_configuration.nix
+++ install_configuration.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ <nixos/modules/installer/cd-dvd/installation-cd-graphical.nix> ];
+  imports = [ <nixos/modules/installer/cd-dvd/installation-cd-minimal.nix> ];
   boot.supportedFilesystems = [ "zfs" ];
 }
