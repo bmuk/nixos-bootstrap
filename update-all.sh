a62a9d89d8481e531e4878c49ff3392d071e3b66
Britt Mathis <britt.mathis@gmail.com>
2015-05-08 17:47:32 -0400

    update all script

---
+++ update-all.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This sshs into all machines and runs the update script
+
+for i in {1..24}; do
+    if [ $i -eq 2 ]
+    then
+        echo "no thinkpad-2"
+    else
+        ssh root@thinkpad-$i 'bash -s' < update.sh
+    fi
+done
