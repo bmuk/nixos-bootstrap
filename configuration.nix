3d44565e9cd0a5fb5c82e0cc2756cd93ca92c126
Britt Mathis <britt.mathis@gmail.com>
2015-06-12 13:37:45 -0400

    testing multiline services

--- configuration.nix
+++ configuration.nix
@@ -64,6 +64,22 @@
     python
   ];
 
+  systemd.services.mesh = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    serviceConfig = {
+      Type = "forking";
+      ExecStart = ''
+        ${pkgs.coreutils}/bin/touch /servicestarted
+        ${pkgs.coreutils}/bin/touch /thisisanotherline
+      '';
+      ExecStop = ''
+        ${pkgs.coreutils}/bin/rm /servicestarted
+        ${pkgs.coreutils}/bin/rm /thisisanotherline
+      '';
+    };
+  };
+
   services = {
     openssh = {
       enable = true;
